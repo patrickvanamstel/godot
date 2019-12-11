93a2ea7e2e6828a2c9673ae51782cc9f0c4535d1
Patrick van Amstel <Patrick.vanAmstel@order2cash.com>
2019-12-11 15:30:31 +0100

    Platformer

---
+++ platformer/src/Actors/Actor.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL = Vector2.UP;
+
+export var jump_force = 1.0
+export var speed: = Vector2(300.0, 3000.0)
+export var gravity: = 3000.0
+var _velocity: = Vector2.ZERO
+
+
+
