355ffff1ae4957a1751ce2199d7289ce3115e772
Patrick van Amstel <Patrick.vanAmstel@order2cash.com>
2019-12-13 13:26:17 +0100

    Initial clone from the platformer game

---
+++ platformer-eyes/src/Actors/Enemy.gd
@@ -0,0 +1,30 @@
+extends "res://src/Actors/Actor.gd"
+
+func _ready() -> void:
+	_velocity.x = -speed.x
+	set_physics_process(false)
+	return
+
+# Signals
+func _on_StompDetector_body_entered(body : PhysicsBody2D):
+	# Needed when dis function is triggered to soon
+	# Second time around it is fine
+	if body == null : 
+		return
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+	
+	pass # Replace with function body.
+
+
+# Call backs on frame level 
+func _physics_process(delta: float) -> void:
+	_velocity.y = _velocity.y + gravity * delta
+	if is_on_wall():
+		_velocity.x *= -1
+	_velocity.y = move_and_slide(_velocity , FLOOR_NORMAL).y
+
+
+
